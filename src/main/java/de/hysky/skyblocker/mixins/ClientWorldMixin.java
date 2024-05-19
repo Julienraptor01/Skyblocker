d4678f54ae604cb7bf638f70fd981c58b37ac7d4
Aaron <51387595+AzureAaron@users.noreply.github.com>
2024-05-19 11:45:42 -0400

    Simon Says Solver

---
+++ src/main/java/de/hysky/skyblocker/mixins/ClientWorldMixin.java
@@ -0,0 +1,25 @@
+package de.hysky.skyblocker.mixins;
+
+import de.hysky.skyblocker.skyblock.dungeon.device.SimonSays;
+import net.minecraft.block.BlockState;
+import net.minecraft.client.world.ClientWorld;
+import net.minecraft.util.math.BlockPos;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import com.llamalad7.mixinextras.sugar.Local;
+
+@Mixin(ClientWorld.class)
+public class ClientWorldMixin {
+
+	/**
+	 * @implNote The {@code pos} can be mutable when this is called by chunk delta updates, so if you want to copy it into memory
+	 * (e.g. store it in a field/list/map) make sure to duplicate it via {@link BlockPos#toImmutable()}.
+	 */
+    @Inject(method = "handleBlockUpdate", at = @At("RETURN"))
+    private void skyblocker$handleBlockUpdate(CallbackInfo ci, @Local(argsOnly = true) BlockPos pos, @Local(argsOnly = true) BlockState state) {
+        SimonSays.onBlockUpdate(pos, state);
+    }
+}
