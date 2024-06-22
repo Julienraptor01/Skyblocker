d6d2281ff0f3e386b278a32bf5b25b9e9d64df1a
Julienraptor01 <julienraptor01@gmail.com>
2024-06-22 05:02:56 +0200

    custom changes

--- src/main/java/de/hysky/skyblocker/skyblock/item/tooltip/adders/LineSmoother.java
+++ src/main/java/de/hysky/skyblocker/skyblock/item/tooltip/adders/LineSmoother.java
@@ -9,25 +9,25 @@ import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 
-public class LineSmoothener extends TooltipAdder {
+public class LineSmoother extends TooltipAdder {
 	//This is static to not create a new text object for each line in every item
-	private static final Text BUMPY_LINE = Text.literal("-----------------").formatted(Formatting.DARK_GRAY, Formatting.STRIKETHROUGH);
+	private static final Text BUMPY_LINE = Text.literal("-".repeat(17)).formatted(Formatting.DARK_GRAY, Formatting.STRIKETHROUGH);
 
-	public static Text createSmoothLine() {
-		return Text.literal("                    ").formatted(Formatting.DARK_GRAY, Formatting.STRIKETHROUGH, Formatting.BOLD);
+	public LineSmoother(int priority) {
+		super(priority);
 	}
 
-	public LineSmoothener() {
-		super(Integer.MIN_VALUE);
+	public static Text createSmoothLine() {
+		return Text.literal(" ".repeat(20)).formatted(Formatting.DARK_GRAY, Formatting.STRIKETHROUGH, Formatting.BOLD);
 	}
 
 	@Override
 	public void addToTooltip(@Nullable Slot focusedSlot, ItemStack stack, List<Text> lines) {
-		for (int i = 0; i < lines.size(); i++) {
-			List<Text> lineSiblings = lines.get(i).getSiblings();
+		for (Text line : lines) {
+			List<Text> lineSiblings = line.getSiblings();
 			//Compare the first sibling rather than the whole object as the style of the root object can change while visually staying the same
 			if (lineSiblings.size() == 1 && lineSiblings.getFirst().equals(BUMPY_LINE)) {
-				lines.set(i, createSmoothLine());
+				lines.set(lines.indexOf(line), createSmoothLine());
 			}
 		}
 	}
